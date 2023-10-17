01f951f22b098714445b7a9f2cf9b143225d8cdf
Antsa-Malala <malalanirina030304@gmail.com>
2023-10-17 19:59:43 +0300

    init-app

---
+++ Firstws/src/main/java/TPWS/Firstws/FirstwsApplication.java
@@ -0,0 +1,13 @@
+package TPWS.Firstws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstwsApplication.class, args);
+	}
+
+}
---
+++ Firstws/src/test/java/TPWS/Firstws/FirstwsApplicationTests.java
@@ -0,0 +1,13 @@
+package TPWS.Firstws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
